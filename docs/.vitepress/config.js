e24ef12d90355853ad68eb6d4cf2a86a9eb131f0
stuyk <stuykgaming@gmail.com>
2023-02-03 13:16:35 -0700

    re-organize top bar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,93 +1,166 @@
 import { updateUserPlugins } from './userPlugins';
 import { getSidebar } from './sidebar';
 
-const BASE_URL = '/'
+const BASE_URL = '/';
 
-export default (async () => {
+export default async () => {
     await updateUserPlugins();
 
     return {
         base: BASE_URL,
-        title: 'Athena Framework',
+        title: 'V5 Docs',
         description: 'Roleplay Script for GTA:V',
         lastUpdated: true,
         cleanUrls: 'without-subfolders',
         markdown: {
             headers: {
-                level: [0, 0]
-            }
+                level: [0, 0],
+            },
         },
         themeConfig: {
             logo: '/logo.png',
             socialLinks: [
                 { icon: 'github', link: 'https://github.com/stuyk/altv-athena' },
-                { icon: 'discord', link: 'https://ares.stuyk.com/v1/get/discord' }
+                { icon: 'discord', link: 'https://ares.stuyk.com/v1/get/discord' },
             ],
             // Navigation - Top of the Documentation Page Links
             nav: [
                 {
-                    text: 'Intro', link: '/intro/intro/index',
-                },
-                {
-                    text: 'API',
+                    text: 'Setup',
                     items: [
                         {
-                            text: 'API Server', link: '/api-server/intro',
+                            text: 'Beginners',
+                            items: [
+                                {
+                                    text: 'Getting Started',
+                                    link: '/intro/intro/index',
+                                },
+
+                            ]
                         },
                         {
-                            text: 'API Client', link: '/api-client/intro',
+                            text: 'Install',
+                            items: [
+                                {
+                                    text: 'Windows',
+                                    link: '/intro/install/installing-on-windows',
+                                },
+                                {
+                                    text: 'Linux',
+                                    link: '/intro/install/installing-on-linux',
+                                }
+                            ]
                         },
                         {
-                            text: 'Plugins', link: '/plugins/about-plugins',
-                        },
-                        {
-                            text: 'Tutorials', link: '/subscriber/tutorials/index',
-                        },
+                            text: 'Mods',
+                            items: [
+                                {
+                                    text: 'About',
+                                    link: '/intro/mods/about-mods',
+                                },
+                                {
+                                    text: 'Adding Mods',
+                                    link: '/intro/mods/adding-mods',
+                                }
+                            ]
+                        }
+
                     ]
+
                 },
                 {
-                    text: 'Plugins',
+                    text: 'Docs',
                     items: [
                         {
-                            text: 'Plugins', link: '/plugins/about-plugins',
+                            text: 'API',
+                            items: [
+                                {
+                                    text: 'API Server',
+                                    link: '/api-server/intro',
+                                },
+                                {
+                                    text: 'API Client',
+                                    link: '/api-client/intro',
+                                },
+                            ]
                         },
                         {
-                            text: 'User Plugins', link: '/user-plugins/index',
+                            text: 'Plugins',
+                            items: [
+                                {
+                                    text: 'About Plugins',
+                                    link: '/plugins/about-plugins',
+                                },
+                                {
+                                    text: 'Creating Plugins',
+                                    link: '/plugins/creating-plugins',
+                                },
+                                {
+                                    text: 'Installing Plugins',
+                                    link: '/plugins/loading-plugins',
+                                },
+                                {
+                                    text: 'User Plugins',
+                                    link: '/user-plugins/index',
+                                },
+                            ]
                         },
-                    ]
+                    ],
                 },
                 {
-                    text: 'Tools',
+                    text: 'Subscribers',
                     items: [
                         {
-                            text: 'Athena Toolkit', link: '/tools/toolkit',
+                            text: 'Articles',
+                            items: [
+                                {
+                                    text: 'Tutorials',
+                                    link: '/subscriber/tutorials/index',
+                                },
+                            ],
                         },
-                    ]
+
+                        {
+                            text: 'Tools',
+                            items: [
+                                {
+                                    text: 'Free - Athena Toolkit',
+                                    link: '/tools/toolkit',
+                                },
+                            ],
+                        },
+                    ],
                 },
                 {
-                    text: 'Version',
+                    text: 'Versions & Links',
                     items: [
                         {
-                            text: 'latest', link: 'https://athenaframework.com',
+                            text: 'Doc Versions',
+                            items: [
+                                {
+                                    text: 'v4 Docs',
+                                    link: 'https://stuyk.github.io/athena-docs-v4/',
+                                },
+                            ],
                         },
                         {
-                            text: 'v4', link: 'https://stuyk.github.io/athena-docs-v4/',
+                            text: 'Partners',
+                            items: [
+                                {
+                                    text: 'alt:V Client',
+                                    link: 'https://altv.mp',
+                                },
+                                {
+                                    text: 'alt:V Native Database',
+                                    link: 'https://natives.altv.mp',
+                                },
+                                {
+                                    text: 'GTA:V Data Browser',
+                                    link: 'https://forge.plebmasters.de/',
+                                },
+                            ],
                         },
-                    ]
-                },
-                {
-                    text: 'Links',
-                    items: [
-                        {
-                            text: 'alt:V Client', link: 'https://altv.mp',
-                        },
-                        {
-                            text: 'alt:V Native Database', link: 'https://natives.altv.mp',
-                        },
-                        {
-                            text: 'GTA:V Data Browser', link: 'https://forge.plebmasters.de/'
-                        },
-                    ]
+                    ],
                 },
             ],
             // Footer - Bottom of the Documentation Page
@@ -97,7 +170,7 @@ export default (async () => {
             // Page - Next & Previous Buttons
             docFooter: {
                 prev: 'Prev',
-                next: 'Next'
+                next: 'Next',
             },
             // Page - Show Last Update on Page
             lastUpdatedText: 'Updated Date',
@@ -110,7 +183,7 @@ export default (async () => {
             ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: BASE_URL + 'favicon-16x16.png' }],
             ['link', { rel: 'manifest', href: BASE_URL + 'site.webmanifest' }],
             ['link', { rel: 'mask-icon', href: BASE_URL + 'safari-pinned-tab.svg', color: '#5bbad5' }],
-            ['meta', { name: 'msapplication-TileColor', content: '#da532c' }]
-        ]
-    }
-});
+            ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+        ],
+    };
+};
